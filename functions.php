02e28e516daa1773d28783146a9bc5d306cea81b
Robciuc Ioan <ionut@9pixels.co>
2015-07-29 08:31:53 +0300

    README.txt and CHANGELOG.txt.

--- functions.php
+++ functions.php
@@ -11,12 +11,32 @@ if( !function_exists( 'rifto_theme_setup' ) ) {
 }
 
 /**
- *	WP Enqueue Style
+ *	Enqueue Style
  */
 if( !function_exists( 'rifto_enqueue_styles' ) ) {
 	add_action( 'wp_enqueue_scripts', 'rifto_enqueue_styles' );
 	function rifto_enqueue_styles() {
         wp_enqueue_style( 'minimalzerif-style', get_stylesheet_directory_uri() . '/style.css', array( 'zerif_style' ), '1.0.0', 'all' );
         wp_enqueue_style( 'zerif_style', get_template_directory_uri() . '/style.css' );
+	}
+}
+
+/**
+ *	Enqueue Scripts
+ */
+if( !function_exists( 'rifto_enqueue_scripts' ) ) {
+	add_action( 'wp_enqueue_scripts', 'rifto_enqueue_scripts' );
+	function rifto_enqueue_scripts() {
+		wp_enqueue_script( 'scripts', get_stylesheet_directory_uri() . '/js/scripts.js', array(), '1.0.0', true );
+	}
+}
+
+/**
+ *	Dequeue Scripts
+ */
+if( !function_exists( 'rifto_dequeue_scripts' ) ) {
+	add_action( 'wp_print_scripts', 'rifto_dequeue_scripts', 100 );
+	function rifto_dequeue_scripts() {
+		wp_dequeue_script( 'zerif_script' );
 	}
 }
